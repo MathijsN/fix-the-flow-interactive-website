750cadd768399c8564afa91905294ad73e2a7c47
MathijsN <72303334+MathijsN@users.noreply.github.com>
2025-12-02 15:59:22 +0100

    created popover animation #7

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,9 @@
 console.log("hello world")
+const popoverCloseIcon = document.querySelector('[popover] svg')
+const popover = document.querySelector('#my-popover')
+
+popoverCloseIcon.addEventListener('click', closePopover)
+
+function closePopover() {
+    popover.hidePopover()
+}
