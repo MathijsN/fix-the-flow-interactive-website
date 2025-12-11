12465b9b1b1abed06146c57fea45eb65842947ef
MathijsN <72303334+MathijsN@users.noreply.github.com>
2025-12-11 10:34:38 +0100

    fixed #19 #25

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,21 @@
 const planVisitButton = document.querySelector('.plan-visit-button')
 const closePopoverButton =  document.querySelector('.close-popover-button')
-const closeMenuButton = document.querySelector('header button')
+const closeMenuButton = document.querySelector('header > button')
 const header = document.querySelector('header')
 
-planVisitButton.addEventListener('click', removeButton)
-closePopoverButton.addEventListener('click', addButton)
+// planVisitButton.addEventListener('click', removeButton)
+// closePopoverButton.addEventListener('click', addButton)
 closeMenuButton.addEventListener('click', toggleMenu)
 
 
 
-function removeButton() {
-    planVisitButton.style.visibility = "hidden";
-}
+// function removeButton() {
+//     planVisitButton.style.visibility = "hidden";
+// }
 
-function addButton() {
-    planVisitButton.style.visibility = "visible";
-}
+// function addButton() {
+//     planVisitButton.style.visibility = "visible";
+// }
 
 function toggleMenu() {
     header.classList.toggle('menu-open')
