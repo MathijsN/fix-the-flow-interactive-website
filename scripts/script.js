478c0e8b4e6e29d74b7751a11be372a1765440e0
MathijsN <72303334+MathijsN@users.noreply.github.com>
2025-12-16 10:33:35 +0100

    fixed #26

--- scripts/script.js
+++ scripts/script.js
@@ -1,22 +1,16 @@
-const planVisitButton = document.querySelector('.plan-visit-button')
-const closePopoverButton =  document.querySelector('.close-popover-button')
 const closeMenuButton = document.querySelector('header > button')
 const header = document.querySelector('header')
+const headerLabel = document.querySelector('header button form label span')
 
-// planVisitButton.addEventListener('click', removeButton)
-// closePopoverButton.addEventListener('click', addButton)
 closeMenuButton.addEventListener('click', toggleMenu)
 
 
-
-// function removeButton() {
-//     planVisitButton.style.visibility = "hidden";
-// }
-
-// function addButton() {
-//     planVisitButton.style.visibility = "visible";
-// }
-
 function toggleMenu() {
     header.classList.toggle('menu-open')
+
+    if (header.classList.contains('menu-open')) {
+        headerLabel.textContent = "Sluit"
+    } else {
+        headerLabel.textContent = "Menu"
+    }
 }
